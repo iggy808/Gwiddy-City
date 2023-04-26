f4e8a8d7fccdc0195295005eb54642576e2cbb11
Spencer <spencerrochel@yahoo.com>
2023-04-26 16:31:15 -0500

    Adjusting script values

--- Assets/Scripts/PlayerActions/PlayerMovement.cs
+++ Assets/Scripts/PlayerActions/PlayerMovement.cs
@@ -28,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
     
     private void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.1f, groundLayer);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
         PlayerInput();
         SpeedControl();
         if (grounded)
