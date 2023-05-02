10da32126f69b6bb210f55258e7481b2d9695321
Spencer <spencerrochel@yahoo.com>
2023-05-02 14:53:03 -0500

    Player movement v2
    Internet is back yaaaay

--- Assets/Scripts/PlayerActions/PlayerMovement.cs
+++ Assets/Scripts/PlayerActions/PlayerMovement.cs
@@ -9,12 +9,18 @@ public class PlayerMovement : MonoBehaviour
     public float groundDrag;
     public float jumpForce;
     public float jumpCooldown;
+    public float dashForce;
+    public float dashCooldown;
     public float airMultiplier;
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode dashKey = KeyCode.LeftShift;
     public LayerMask groundLayer;
     public Transform orientation;
     private bool grounded;
     private bool jumpReady = true;
+    private bool dashReady = true;
+    private bool dashCD = true;
+    private int jumps;
     private float horizontalInput;
     private float verticalInput;
     private Vector3 moveDirection;
@@ -32,7 +38,12 @@ public class PlayerMovement : MonoBehaviour
         PlayerInput();
         SpeedControl();
         if (grounded)
+        {
             rb.drag = groundDrag;
+            jumps = 0;
+            if (dashCD)
+                dashReady = true;
+        }
         else
             rb.drag = 0;
     }
@@ -46,11 +57,22 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        if(Input.GetKey(jumpKey) && jumpReady && grounded)
+        if(Input.GetKey(jumpKey) && jumpReady)
         {
-            jumpReady = false;
-            Jump();
-            Invoke("ResetJump", jumpCooldown);
+            if (jumps != 1)
+            {
+                jumpReady = false;
+                jumps += 1;
+                Jump();
+                Invoke("ResetJump", jumpCooldown);
+            }
+        }
+        if(Input.GetKey(dashKey) && dashReady)
+        {
+            dashReady = false;
+            dashCD = false;
+            Dash();
+            Invoke("ResetDash", dashCooldown);
         }
     }
 
@@ -82,5 +104,16 @@ public class PlayerMovement : MonoBehaviour
     private void ResetJump()
     {
         jumpReady = true;
+    }
+
+    private void Dash()
+    {
+        rb.velocity = new Vector3(0, 0, 0);
+        rb.AddRelativeForce(orientation.forward * dashForce, ForceMode.Impulse);
+    }
+
+    private void ResetDash()
+    {
+        dashCD = true;
     }
 }
