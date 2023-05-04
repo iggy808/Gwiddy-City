d772378ec00f615f714466c122f369f78bd47338
ColinBumpass <57447291+ReallyNotColin@users.noreply.github.com>
2023-05-04 07:23:44 -0500

    Created collectable orb and pickup sound need to attach sound

---
+++ Assets/Scripts/PlayerActions/PlayerOrbCollection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOrbCollection : MonoBehaviour
+{
+
+    public int total;
+    // Start is called before the first frame update
+    void Start()
+    {
+        total = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(total);
+    }
+}
