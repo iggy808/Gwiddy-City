205e48ac03e7f73745686dee290c5470de434518
Eruruw <spencerrochel@yahoo.com>
2023-05-08 19:24:54 -0500

    Jump glitch fixed?

--- Assets/Scripts/PlayerActions/PlayerMovement.cs
+++ Assets/Scripts/PlayerActions/PlayerMovement.cs
@@ -35,11 +35,9 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
     }
 
-
-
     private void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.8f, groundLayer);
         PlayerInput();
         SpeedControl();
         if (grounded)
@@ -51,9 +49,6 @@ public class PlayerMovement : MonoBehaviour
         }
         else
             rb.drag = 0;
-
-
-
     }
 
     
