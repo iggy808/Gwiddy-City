e0ba6bbec31d2a233e4eed018c1403a07f6afa48
Eruruw <spencerrochel@yahoo.com>
2023-05-10 17:57:44 -0500

    Pause menu v1

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject menuUI;
+    public PlayerCam playerCam;
+    public bool paused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+                Pause();
+            else
+                UnPause();
+        }    
+    }
+
+    public void Pause()
+    {
+        playerCam.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        menuUI.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void UnPause()
+    {
+        playerCam.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        menuUI.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    public void Return()
+    {
+        UnPause();
+        //SceneManager.LoadScene("Main Menu");
+    }
+}
