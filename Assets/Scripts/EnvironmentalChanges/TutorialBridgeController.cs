8e1539749f3a00ba9ecd5439818526117fcce412
evangray7373@gmail.com <evangray7373@gmail.com>
2023-05-14 16:23:46 -0500

    Updated bridge platforms - working on second island in tutorial level

--- Assets/Scripts/EnvironmentalChanges/TutorialBridgeController.cs
+++ Assets/Scripts/EnvironmentalChanges/TutorialBridgeController.cs
@@ -7,13 +7,16 @@ public class TutorialBridgeController : MonoBehaviour
 	[SerializeField]
 	List<GameObject> Platforms;
 
+	float heightOffset = 2f;
 	public void RaiseBridge()
 	{
+		int i = 1;
 		foreach (var platform in Platforms)
 		{
 			Transform platformTransform = platform.GetComponent<Transform>();
-			platformTransform.position = new Vector3(platformTransform.position.x, platformTransform.position.y + 4.91f * 4, platformTransform.position.z);
+			platformTransform.position = new Vector3(platformTransform.position.x, platformTransform.position.y + 4.91f * 4 + (i*heightOffset), platformTransform.position.z);
 			//StartCoroutine(RaiseFashionably(platformTransform));
+			i++;
 		}
 	}
 
